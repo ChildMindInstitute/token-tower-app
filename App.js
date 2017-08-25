7b6c627c37b4f7f87cb4daf1d460bc0588cc41db
MarcoSamson21 <marco.samson21@gmail.com>
2017-08-25 04:50:45 -0400

    add animations for putting coin

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Image, Animated } from 'react-native';
 
 import images from './app/Resources/Images'
 
 export default class App extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidUpdate() {
+    if (this.state.putCoint)
+      Animated.timing(                  // Animate over time
+        this.state.fadeAnim,            // The animated value to drive
+        {
+          toValue: 1,                   // Animate to opacity: 1 (opaque)
+          duration: 1000,              // Make it take a while
+        }
+      ).start(() => { this.setState({ putCoint: false, fadeAnim: new Animated.Value(0) }) });
+  }
+
+  _onPigPress = () => {
+    if (!this.state.putCoint) this.setState({ putCoint: true });
+  }
+
   render() {
     return (
       <Image source={images.firstbackground} style={styles.bgrContainer}>
         <Image source={images.token} />
-        {this.state.putCoint && <Image source={images.k1} />}
+        {this.state.putCoint &&
+          <Animated.Image source={images.k1} style={{
+            opacity: this.state.fadeAnim,
+            transform: [{
+              translateY: this.state.fadeAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 350]
+              }),
+            }],
+          }} />}
         <TouchableOpacity onPress={this._onPigPress}>
           <Image source={images.pig} />
         </TouchableOpacity>
@@ -24,6 +52,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingTop: 20,
     width: null,
-    height: null    
+    height: null
   }
 });
