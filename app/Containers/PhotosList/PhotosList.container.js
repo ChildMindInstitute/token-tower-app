f28e1b08f95ad347e250f13f7395c6ed9fde83ed
MarcoSamson21 <marco.samson21@gmail.com>
2017-10-17 20:51:47 -0400

    Photo list

--- app/Containers/PhotosList/PhotosList.container.js
+++ app/Containers/PhotosList/PhotosList.container.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { View, Text, Image } from 'react-native';
+import { connect } from 'react-redux';
+import PhotoGrid from 'react-native-photo-grid';
 
+import Header from '../../Components/TokenTotemHeader/TokenTotemHeader.component';
+import Btn from '../../Components/FormButton/FormButton.component';
 
-class PhotosListContainer extends React.Component {
-  componentDidMount() {
+import styles from './PhotosList.container.styles';
+
+import { DIRECTION } from '../../Utilities/Constant.utils';
+
+class PhotosListContainer extends Component {
+  _renderHeader = () => (
+    <Text>Please select your photos to delete!</Text>
+  );
+
+  _onPhotoPress = () => {
 
   }
 
-  render() {
+  _renderItem = (item, itemSize) => {
+    const onPress = () => {
+      this._onPhotoPress(item);
+    };
+
     return (
-      <View />
+      <Btn
+        key={item.id} onPress={onPress}
+        btnStyle={{ width: itemSize, height: itemSize }}
+      >
+        <Image
+          resizeMode="contain"
+          style={{ flex: 1 }}
+          source={{ uri: item.src }}
+        />
+      </Btn>
+    );
+  }
+
+  render() {
+    const { photoList } = this.props;
+    const imgKeylist = Object.keys(photoList);
+    const data = imgKeylist.map(key => (
+      { id: key, src: photoList[key].tokenImgUrl }
+    ));
+
+    return (
+      <View style={styles._container}>
+        <Header direction={DIRECTION.HORIZONTAL} />
+        <PhotoGrid
+          data={data}
+          itemsPerRow={3}
+          itemMargin={1}
+          renderHeader={this._renderHeader}
+          renderItem={this._renderItem}
+        />
+      </View>
     );
   }
 }
 
-export default PhotosListContainer;
+const mapStateToProps = state => ({
+  user: state.user,
+  photoList: state.photo
+});
+
+const mapDispatchToProps = {
+};
+
+PhotosListContainer.propTypes = {
+  photoList: propTypes.arrayOf(propTypes.object)
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhotosListContainer);
---
+++ app/Containers/PhotosList/PhotosList.container.styles.js
@@ -0,0 +1,8 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  _container: {
+    flex: 1,
+    // paddingHorizontal: 30
+  }
+});
